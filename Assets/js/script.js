9e78afa908c67ff2d524852d4c3b741fc7c7b5fd
William Nicholson <will98nicholson@gmail.com>
2021-02-16 15:36:56 -0500

    questions and some variables in js

---
+++ Assets/js/script.js
@@ -0,0 +1,25 @@
+var questions = [
+    {
+        title: "The data type that contains one of two possible values, usually True or False, is known as:",
+        choices: ["string", "boolean", "number", "integer"],
+        answer: "boolean"
+    },
+    {
+        title: "Which of the following is the coding language that is used to style and format the layout of a webpage?",
+        choices: ["CSS", "Javascript", "HTML", "HooliTech"],
+        answer: "CSS"
+    },
+    {
+        title: "Which HTML element tag is generally the most useful when dealing with Flexboxes?",
+        choices: ["head", "div", "body", "footer"],
+        answer: "div"
+    },
+    {
+        title: "In Javascript, values of an array must be enclosed by:",
+        choices: ["brackets", "question marks", "quotation marks", "parentheses"],
+        answer: "brackets"
+    },
+
+];
+
+
